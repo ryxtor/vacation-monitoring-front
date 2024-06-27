0bbfabc1db60fa870aac4b732983d2f8168b0b75
Lucas Bonnefon <ryxtor@outlook.com>
2024-06-27 20:27:20 -0300

    Avoid redirect to login if you are already there

--- src/api/axios.js
+++ src/api/axios.js
@@ -30,7 +30,9 @@ instance.interceptors.response.use(
   (error) => {
     if (error.response && error.response.status === 401) {
       localStorage.removeItem('token');
-      window.location.href = '/login';
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
     }
     return Promise.reject(error);
   }
